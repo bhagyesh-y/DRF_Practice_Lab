24263911093e2c2cfdb5f8179419f39432e7a202
bhagyesh508 <bhagyeshyadav29@gmail.com>
2026-01-18 12:44:54 +0530

    crud operations in function based views

--- Rest_main/testapp/views.py
+++ Rest_main/testapp/views.py
@@ -20,12 +20,22 @@ def students(request):
         return Response(serializer.errors , status=status.HTTP_400_BAD_REQUEST)
     
 
-@api_view(['GET','POST'])
+@api_view(['GET','PUT','DELETE'])
 def studentdetailview(request,pk):
+    try:
+        student = Student.objects.get(pk=pk)
+    except Student.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
     if request.method == 'GET':
-        try:
-           student = Student.objects.get(pk=pk)
-           serializer = StudentSerializer(student)
-           return Response(serializer.data,status=status.HTTP_200_OK)
-        except Student.DoesNotExist:
-            return Response(status=status.HTTP_404_NOT_FOUND)
+        serializer = StudentSerializer(student)
+        return Response(serializer.data,status=status.HTTP_200_OK)
+    elif request.method=='PUT':
+            serializer=StudentSerializer(student,data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data,status=status.HTTP_200_OK)
+            else:
+                return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)    
+    elif request.method == 'DELETE':
+        student.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)        
